dee01625ef9ebff40292e5ee2dc7de89d1e1fc0f
Mandeep Singh Kapoor <mandeepkapoor.aws@gmail.com>
2022-07-26 17:15:58 +0530

    Create ec2-bad-example.tf

---
+++ ec2-bad-example.tf
@@ -0,0 +1,16 @@
+ resource "aws_instance" "bad_example" {
+
+     ami           = "ami-12345667"
+     instance_type = "t2.small"
+
+     user_data = <<EOF
+ export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+ export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+ export AWS_DEFAULT_REGION=us-west-2 
+ EOF
+ }
+ export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+ export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+ export AWS_DEFAULT_REGION=us-west-2 
+ EOF
+ }
