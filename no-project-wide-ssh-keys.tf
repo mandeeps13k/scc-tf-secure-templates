06f311cbfc61992388933dfc439ceb2ad9f757f0
Mandeep Kapoor <mandeep.kapoor@commercetools.com>
2022-07-27 04:36:03 +0530

    test

--- no-project-wide-ssh-keys.tf
+++ no-project-wide-ssh-keys.tf
@@ -1,43 +1,43 @@
 resource "google_service_account" "default" {
-   account_id   = "service_account_id"
-   display_name = "Service Account"
- }
+  account_id   = "service_account_id"
+  display_name = "Service Account"
+}
 
- resource "google_compute_instance" "default" {
-   name         = "test"
-   machine_type = "e2-medium"
-   zone         = "us-central1-a"
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
-   tags = ["foo", "bar"]
+  tags = ["foo", "bar"]
 
-   boot_disk {
-     initialize_params {
-       image = "debian-cloud/debian-9"
-     }
-   }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
-   // Local SSD disk
-   scratch_disk {
-     interface = "SCSI"
-   }
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
 
-   network_interface {
-     network = "default"
+  network_interface {
+    network = "default"
 
-     access_config {
-       // Ephemeral IP
-     }
-   }
-# NO Project Wide SSH Keys
-   metadata = {
-     block-project-ssh-keys = true
-   }
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  # NO Project Wide SSH Keys
+  metadata = {
+    block-project-ssh-keys = true
+  }
 
-   metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = "echo hi > /test.txt"
 
-   service_account {
-     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-     email  = google_service_account.default.email
-     scopes = ["cloud-platform"]
-   }
- }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
--- project-level-oslogin.tf
+++ project-level-oslogin.tf
@@ -1,5 +1,5 @@
- resource "google_compute_project_metadata" "default" {
-   metadata = {
-     enable-oslogin = true
-   }
- }
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    enable-oslogin = true
+  }
+}
